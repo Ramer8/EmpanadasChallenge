9cf12636de8ba331d5725a5bacd4eb6b09416ee0
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-02-16 00:00:33 +0100

    another equation to test the function

--- main.js
+++ main.js
@@ -17,7 +17,16 @@ export const fn = (a, b, c) => {
   if (a < 0 || b < 0 || c < 0 || a + b + c >= 40 || (a + b + c) % 3 !== 0) {
     throw Error
   } else {
-    total = ((a * pa) / 2 + 0 * pb + (c * pc) / 2 + pb * b) / 3
+    total = (a * pa + b * pb + c * pc) / 3
   }
   return total
 }
+// ✓ main.test.js (7)
+//    ✓ Function fn - Input Parameter Test Cases (7)
+//      ✓ Case: fn(1, 1, 1) - Expected Result: 14
+//      ✓ Case: fn(3, 3, 0) - Expected Result: 26
+//      ✓ Case: fn(2, 0, 1) - Expected Result: 14
+//      ✓ Case: fn(9, 10, 11) - Expected Result: 142
+//      ✓ Throw error on negative input value: fn(-1, 3, 1)
+//      ✓ Throws error when the total sum of inputs (2+2+1) is not a multiple of 3: fn(2, 2, 1)
+//      ✓ Throws error when the total sum of inputs (15+15+20) is not less than 40: fn(15, 15, 20)
