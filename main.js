e12f73297e0405f8a20431891f6b48a16e9a633e
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-02-17 09:36:35 +0100

    another equation to test the function

--- main.js
+++ main.js
@@ -1,32 +1,20 @@
-// export const fn = (/* argumentos de la función */) => {
-//   // Cuerpo de la función: Completa la lógica de la función aquí.
-// }
-// /
-// //
-// La función debe tener tres parámetros de entrada. Cada conjunto de entrada consta
-// de tres enteros, a b c, que son el número de empanadas de 3, 4 y 5 ingredientes,
-// respectivamente. Se garantiza que el número de empanadas, a + b + c, cumple que
-// a + b + c < 40. Además, el número de empanadas es divisible entre 3.
-// //
+// Cuerpo de la función: Completa la lógica de la función aquí.
+// export const fn = (/* argumentos de la función */) => {}
+//
+// Argumento de la funcion:
+// a = Cantidad de emanadas de Pollo
+// b = Cantidad de empanadas de Carne
+// c = Cantidas de empanadas Vegetariana
+//
 export const fn = (a, b, c) => {
   let pa = 12 // Precio Empanada de Pollo
   let pb = 14 // Precio Empanada de Carne
   let pc = 16 // Precio Empanada Vegetariana
   let total
-  console.log("casa")
   if (a < 0 || b < 0 || c < 0 || a + b + c >= 40 || (a + b + c) % 3 !== 0) {
     throw Error
   } else {
-    total = (a * pa + b * pb + c * pc) / 3
+    total = Math.ceil((a * pa + b * pb + c * pc) / 3)
   }
   return total
 }
-// ✓ main.test.js (7)
-//    ✓ Function fn - Input Parameter Test Cases (7)
-//      ✓ Case: fn(1, 1, 1) - Expected Result: 14
-//      ✓ Case: fn(3, 3, 0) - Expected Result: 26
-//      ✓ Case: fn(2, 0, 1) - Expected Result: 14
-//      ✓ Case: fn(9, 10, 11) - Expected Result: 142
-//      ✓ Throw error on negative input value: fn(-1, 3, 1)
-//      ✓ Throws error when the total sum of inputs (2+2+1) is not a multiple of 3: fn(2, 2, 1)
-//      ✓ Throws error when the total sum of inputs (15+15+20) is not less than 40: fn(15, 15, 20)
